15c1cef3c8bb5954781e772a228d8cd7e8560a79
zyzzyva <foad@nty.cumshark>
2025-06-02 20:45:45 -0700

    uhhhhh

--- vf-math-train-6node.sh
+++ vf-math-train-6node.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ##vf-math-train-6node.sh
 source .venv/bin/activate
+export OPENAI_API_KEY=FRICK0AWAY0AND0BEGONE
 # Run training script from verifiers/, with .venv active
 #try running on 6node
-CUDA_VISIBLE_DEVICES=3,4,5 accelerate launch --num-processes 2 --config-file configs/zero3.yaml verifiers/examples/soft_math_train.py
+CUDA_VISIBLE_DEVICES=3,4,5 accelerate launch --num-processes 3 --config-file configs/zero3.yaml verifiers/examples/soft_math_train.py
--- vf-vllm-6node-host-qw3-06b.sh
+++ vf-vllm-6node-host-qw3-06b.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 ##vf-vllm-6node-host-qw3-06b.sh
 source .venv/bin/activate
+export OPENAI_API_KEY=FRICK0AWAY0AND0BEGONE
 # Launch vLLM inference server from verifiers/, with .venv active
 #https://huggingface.co/Qwen/Qwen3-0.6B-Base
 #tp=1 bc its 0.6b.
 #try running on 6node
 CUDA_VISIBLE_DEVICES=0,1,2 vf-vllm \
 --model "Qwen/Qwen3-0.6B-Base" \
---tensor_parallel_size 1 --max_model_len 8192 \
---gpu_memory_utilization 0.9 --enable_prefix_caching True \
+--tensor-parallel-size 1 --max-model-len 8192 \
+--gpu-memory-utilization 0.9 --enable-prefix-caching \
 --dtype bfloat16 \
 --host 0.0.0.0 --port 8000
