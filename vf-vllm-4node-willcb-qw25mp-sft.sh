675540f8c8da3d1a07fcbd268cb6aed140888b5a
zyzzyva <foad@nty.cumshark>
2025-06-04 14:59:49 -0700

    yes same as  before yes of course

--- vf-vllm-4node-willcb-qw25mp-sft.sh
+++ vf-vllm-4node-willcb-qw25mp-sft.sh
@@ -7,6 +7,6 @@ export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
 
 CUDA_VISIBLE_DEVICES=0,1 vf-vllm --model "willcb/Qwen2.5-7B-Math-Python-SFT" \
     --tensor-parallel-size 4 --max-model-len 8192 --dtype bfloat16 \
-    --gpu-memory-utilization 0.9 --enable-prefix-caching True \
+    --gpu-memory-utilization 0.9 --enable-prefix-caching \
     --host 0.0.0.0 --port 8000
 #CUDA_VISIBLE_DEVICES=3,4 accelerate launch --config-file configs/zero3.yaml verifiers/examples/math_python_4node.py
