50b72d0be569216f180681176fce87051c1a8402
zyzzyva <foad@nty.cumshark>
2025-06-04 15:24:10 -0700

    as usual it is revealed that every single difference between my reference implementation and the upstream examples is mandatory, not optional; for every commit in the line towards this ersino of vf-math-python-4node.sh does not even run. like. at all.

--- vf-math-python-4node.sh
+++ vf-math-python-4node.sh
@@ -2,12 +2,12 @@
 ##vf-math-python-4node.sh
 source .venv/bin/activate
 export OPENAI_API_KEY=FRICK0AWAY0AND0BEGONE
-#export WANDB_DISABLED=true
-export WANDB_OFFLINE=true
+export WANDB_DISABLED=true
+#export WANDB_OFFLINE=true
 #...
 
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python verifiers/inference/vllm_serve.py --model "willcb/Qwen2.5-7B-Math-Python-SFT" \
 #    --tensor_parallel_size 4 --max_model_len 8192 --dtype bfloat16 \
 #    --gpu_memory_utilization 0.9 --enable_prefix_caching True \
 #    --host 0.0.0.0 --port 8000
-CUDA_VISIBLE_DEVICES=2,3 accelerate launch --config-file configs/zero3.yaml verifiers/examples/math_python_4node.py
+CUDA_VISIBLE_DEVICES=2,3 accelerate launch --num-processes 2 --config-file configs/zero3.yaml verifiers/examples/math_python_4node.py
